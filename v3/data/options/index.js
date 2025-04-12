d33e0d3de6379da4ddfb6ab24199747f9a6d9c0b
Joe Ertaba <belaviyo@gmail.com>
2025-04-12 04:32:52 -0700

    version 0.1.9

--- v3/data/options/index.js
+++ v3/data/options/index.js
@@ -21,3 +21,9 @@ document.forms[0].onsubmit = async e => {
     self.toast.textContent = '';
   }, 750);
 };
+// links
+for (const a of [...document.querySelectorAll('[data-href]')]) {
+  if (a.hasAttribute('href') === false) {
+    a.href = chrome.runtime.getManifest().homepage_url + '#' + a.dataset.href;
+  }
+}
